0353c171c0d8b69238d2894448cdb773c440ae4b
pranshut111 <115647024+pranshut111@users.noreply.github.com>
2026-01-29 12:53:31 +0530

    Add files via upload

---
+++ QE-Java/DemoPackageRunner.java
@@ -0,0 +1,9 @@
+package TestRunner;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+public class DemoPackageRunner {
+
+}
---
+++ QE-Java/TestCalculator.java
@@ -0,0 +1,21 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(4, calculator.add(2, 2));
+    }
+}
